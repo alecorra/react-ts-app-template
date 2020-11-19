577b0c8f05f92f3aee883156f357fd3260bfac8c
Alessandro Corra <alessandro.corra@lovehoney.co.uk>
2020-11-19 12:03:06 +0000

    jest configured

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find('.App').exists()).toBe(true);
+  });
 });
